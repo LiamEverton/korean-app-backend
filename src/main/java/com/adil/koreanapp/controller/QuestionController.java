73051c7cea2180590e26281fbbe553e0b5d9f2b7
AdilahmedDev <80524770+AdilahmedDev@users.noreply.github.com>
2021-09-01 20:52:58 +0100

    added rest endpoints back

--- src/main/java/com/adil/koreanapp/controller/QuestionController.java
+++ src/main/java/com/adil/koreanapp/controller/QuestionController.java
@@ -1,6 +1,8 @@
 package com.adil.koreanapp.controller;
 
 import com.adil.koreanapp.model.Question;
+import com.adil.koreanapp.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,32 +14,21 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping("/questions")
 public class QuestionController {
-//    Questions questions = new Questions(new ArrayList<Question>());
+    @Autowired
+    QuestionRepository questionRepository;
 
-//    @GetMapping(value = "/question/{id}", produces = "application/json")
-//    public @ResponseBody
-//    Question getQuestion(@PathVariable int id) {
-//        return questions.getQuestion(id);
-//    }
+    @GetMapping(value = "/question/{id}", produces = "application/json")
+    public @ResponseBody
+    Question getQuestion(@PathVariable int id) {
+        return questionRepository.findById((long) id).get();
+    }
 
-//    @GetMapping(value = "/questions", produces = "application/json")
-//    public @ResponseBody
-//    Questions getQuestions() {
-//        return questions;
-//    }
 
-//    @PostMapping(value = "/question/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity postQuestion(@RequestBody Question question){
-//        System.out.println("add question");
-//        questions.addQuestion(question);
-//        return new ResponseEntity<Question>(questions.getQuestion(0),HttpStatus.CREATED);
-//
-//    }
-//    @PostMapping(value = "/questions", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity<Questions> postQuestions(@RequestBody Questions questions){
-//        System.out.println("add question");
-//        this.questions = questions;
-//        return new ResponseEntity<Questions>(this.questions,HttpStatus.CREATED);
-//
-//    }
+    @PostMapping(value = "/question/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity postQuestion(@RequestBody Question question){
+        Question q = questionRepository.save(question);
+        return new ResponseEntity<Question>(q,HttpStatus.CREATED);
+
+    }
+
 }
